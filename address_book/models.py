5cb5458d2f8a0553eededc8335f0d38646f6d488
Murilo Camargos <murilo@ckl.io>
2018-03-06 20:41:08 -0300

    remove old address table and its relationship to client

--- address_book/models.py
+++ address_book/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Address(models.Model):
-    street_name = models.CharField(max_length=255)
-
 class Client(models.Model):
     name = models.CharField(max_length=255)
-    addresses = models.ManyToManyField(Address)
 
 class NewAddress(models.Model):
     street_name = models.CharField(max_length=255)
