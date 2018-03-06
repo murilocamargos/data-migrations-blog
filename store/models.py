6c38dde2f15e569dc8d03df3743e7be331b61147
Murilo Camargos <murilo@ckl.io>
2018-03-06 20:30:51 -0300

    add computer store first migration

--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+
+class Computer(models.Model):
+    brand = models.CharField(max_length=255)
+    bought_by = models.ForeignKey(Client, related_name='computers')
